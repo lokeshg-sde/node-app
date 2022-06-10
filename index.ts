2935ae35d59652f44eefc925fd975f568589dfa9
lokeshg-fsd <lokesh.jejappa@gmail.com>
2022-06-10 13:25:22 +0530

    Add Authorization Layer

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 import env from 'dotenv'
-
+import unless from 'express-unless'
 import express from 'express'
 import mongoose from 'mongoose'
+
+import { authenticateToken } from './src/helpers/jwt'
 import routes from './src/routes'
 
 env.config()
@@ -10,6 +12,16 @@ const mongoString: string = process.env.DATABASE_URL as string
 const portNumber: number | string = process.env.PORT || 3000
 const app = express()
 
+// middleware for authenticating token submitted with requests
+authenticateToken.unless = unless
+app.use(
+  authenticateToken.unless({
+    path: [
+      { url: '/users/login', methods: ['POST'] },
+      { url: '/users/register', methods: ['POST'] }
+    ]
+  })
+)
 app.use(express.json())
 app.use('/', routes)
 

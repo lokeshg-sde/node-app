5f73b8324843297d993e974495cfed5d2df7fb9e
lokeshg-fsd <lokesh.jejappa@gmail.com>
2022-06-21 23:20:34 +0530

    feat: Create Product Model

---
+++ src/models/product.ts
@@ -0,0 +1,20 @@
+import { Schema, model } from 'mongoose'
+
+const ProductSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true
+    },
+    type: String,
+    name: {
+      type: String
+    },
+    category: String
+  },
+  { timestamps: true }
+)
+
+const Product = model('product', ProductSchema)
+
+export default Product

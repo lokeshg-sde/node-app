b27d66bd82ba5065f522018001d2de65f802078b
lokeshg-fsd <lokesh.jejappa@gmail.com>
2022-06-24 20:27:27 +0530

    Remove Static Build Directory from repo

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import path from 'path'
 
 import { PORT_NUMBER, DATABASE_URL, URL_CONFIG } from './src/config'
 import { authenticateToken } from './src/auth/jwt'
@@ -9,32 +10,27 @@ import routes from './src/routes'
 
 const app = express()
 
-const path = `${__dirname}/client/build/`
+const buildDir = [__dirname, 'client', 'build']
+const buildPublicDir = [__dirname, 'client', 'public']
+const publicDir = [__dirname, 'public']
 
 // middleware for authenticating token submitted with requests
 app.use(express.json())
-app.use(express.static(path))
+app.use(express.static(path.join(...buildDir)))
+app.use(express.static(path.join(...buildPublicDir)))
+app.use(express.static(path.join(...publicDir)))
 
 app.use(excludePublicUrlOnAuthenticate(URL_CONFIG, authenticateToken))
 app.get('/home', (req, res) => {
-  res.sendFile(path, 'index.html')
+  res.sendFile(path.join(...buildDir, 'index.html'))
 })
 app.use('/', routes)
-app.get('/', (req, res)=> {
+app.get('/', (req, res) => {
   res.redirect('/home')
 })
 
 mongoose.connect(DATABASE_URL)
-// .then(() => {
-//   app.listen(portNumber, () => {
-//     console.log('Database Connected')
-//     console.log(`Server Started at ${portNumber}`)
-//   })
-// })
-// .catch((error) => {
-//   console.log(error.message)
-//   console.log('URI: ' + mongoString)
-// })
+
 const database = mongoose.connection
 
 database.on('error', (error: unknown) => {

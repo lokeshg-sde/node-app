e3d3dc110bfe7036a07a42ed44220dc3d8d4ff45
lokeshg-fsd <lokesh.jejappa@gmail.com>
2022-06-28 12:24:22 +0530

    Fix Port issue on Production

--- src/config/environmentVariables.ts
+++ src/config/environmentVariables.ts
@@ -4,6 +4,6 @@ config()
 
 const defaultPort = 5000
 
-export const PORT_NUMBER = (process.env.PORT_NUMBER || defaultPort) as number
+export const PORT_NUMBER = (process.env.PORT_NUMBER || process.env.PORT || defaultPort) as number
 export const JWT_TOKEN_SECRET = process.env.JWT_TOKEN_SECRET as string
 export const DATABASE_URL = process.env.DATABASE_URL as string

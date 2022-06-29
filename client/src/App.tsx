e3b1423c97252d1febcd861b0a0a070eaafcb4cd
lokeshg-fsd <lokesh.jejappa@gmail.com>
2022-06-29 13:26:20 +0530

    feat/ Update package.JOSN and add material UI styled components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,21 @@
 import React from 'react'
 import logo from './logo.svg'
+import Icon from './logo.svg'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={'/favicon.ico'} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <img src={Icon} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Node JS App built with <strong>ReactJS</strong> Library as <code>Fron End</code> and{' '}
+          <strong>ExpressJS and MongoDB</strong> as
+          <code> Back End</code>.
         </p>
-        <a
-          className="App-link"
-          href={logo}
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
+        <a className="App-link" href={logo} target="_blank" rel="noopener noreferrer">
+          ReactJS
         </a>
       </header>
     </div>

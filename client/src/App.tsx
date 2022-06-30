6671a66456a0a61d834eb1b40d4f08a654cadb82
lokeshg-fsd <lokesh.jejappa@gmail.com>
2022-06-30 12:19:31 +0530

    Add Login And Sign Up Page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,29 @@
-import React from 'react'
-import logo from './logo.svg'
-import Icon from './logo.svg'
-import './App.css'
+import { useState } from 'react'
+import Button from '@material-ui/core/Button'
+import ModalDialog from './components/ModalDialog'
+import LoginForm from './components/LoginForm'
 
-function App() {
+const App = () => {
+  // declare a new state variable for modal open
+  const [open, setOpen] = useState(false)
+
+  // function to handle modal open
+  const handleOpen = () => {
+    setOpen(true)
+  }
+
+  // function to handle modal close
+  const handleClose = () => {
+    setOpen(false)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={'/d.ico'} alt="logo" />
-        <img src={Icon} className="App-logo" alt="logo" />
-        <p>
-          Node JS App built with <strong>ReactJS</strong> Library as <code>Fron End</code> and{' '}
-          <strong>ExpressJS and MongoDB</strong> as
-          <code> Back End</code>.
-        </p>
-        <a className="App-link" href={logo} target="_blank" rel="noopener noreferrer">
-          ReactJS
-        </a>
-      </header>
+      <LoginForm />
+      <Button variant="contained" color="primary" onClick={handleOpen}>
+        Signup
+      </Button>
+      <ModalDialog open={open} handleClose={handleClose} />
     </div>
   )
 }
---
+++ client/src/components/ModalDialog.tsx
@@ -0,0 +1,16 @@
+import React, { useRef } from 'react'
+import Dialog from '@material-ui/core/Dialog'
+import Form from './SignUpForm'
+
+const ModalDialog = ({ open, handleClose }: any) => {
+  const ref = useRef()
+
+  return (
+    // props received from App.js
+    <Dialog ref={ref} open={open} onClose={handleClose}>
+      <Form handleClose={handleClose} />
+    </Dialog>
+  )
+}
+
+export default ModalDialog

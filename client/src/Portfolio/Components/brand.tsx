892c8cc608daa291ff3ec67932f9be3166265bce
lokeshg-fsd <lokesh.jejappa@gmail.com>
2022-08-07 02:04:38 +0530

    Remeove the UnUsed Assets

--- client/src/Portfolio/Components/brand.tsx
+++ client/src/Portfolio/Components/brand.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import styled from '@emotion/styled'
 
-import logo from './default-portfolio-logo.png'
+import logo from './logo.png'
 
 const Logo = styled.img`
+  margin-left: 2rem;
   width: 4.5rem;
   height: 4.5rem;
   transform: rotate(21deg);

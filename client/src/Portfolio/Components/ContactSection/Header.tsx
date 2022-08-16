e177e1ccc7c733d8bda66428a56a1384f9fa2080
lokeshg-fsd <lokesh.jejappa@gmail.com>
2022-08-16 12:08:54 +0530

    Add Components Required for contact section

---
+++ client/src/Portfolio/Components/ContactSection/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import { HeaderText, TextWrapper } from './styled'
+
+export const Header = (): JSX.Element => (
+  <HeaderText>
+    <TextWrapper>
+      I am available to work on your Organization/Projects and bring your ideas to life. I am just a
+      click away.
+    </TextWrapper>
+  </HeaderText>
+)
---
+++ client/src/Portfolio/Components/ContactSection/index.tsx
@@ -0,0 +1,3 @@
+export * from './Header'
+export * from './Details'
+export * from './styled'

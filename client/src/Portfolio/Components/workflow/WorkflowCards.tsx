7268f26e618cef5902f73cb82143d95ec1478d05
lokeshg-fsd <lokesh.jejappa@gmail.com>
2022-08-20 20:12:42 +0530

    Add Proxy server Path to React Client

--- client/src/Portfolio/Components/workflow/WorkflowCards.tsx
+++ client/src/Portfolio/Components/workflow/WorkflowCards.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 
+import { getApiConfig } from '../../../utils/getApiConfig'
 import { Wrapper } from './styledComponents'
 import Card from './workflowCard'
+
+const origin = getApiConfig()
 
 const cardsData = [
   { title: 'Discover', info: 'I conduct user research to identify the problem I want to solve.' },
   { title: 'Define', info: 'I brainstorm possible design solutions to the identified problem.' },
   {
     title: 'Ideate',
+    source: `${origin}/assets/reactjs-svgrepo-com.svg`,
     info: 'I create wire frames and sketches of the product I’m about to design.',
   },
   { title: 'Prototype', info: 'I create high fidelity design and prototype the screens.' },
@@ -20,8 +24,8 @@ const cardsData = [
 
 export const WorkFlowCards = (): JSX.Element => (
   <Wrapper>
-    {cardsData.map(({ title, info, padding }) => (
-      <Card key={title} info={info} padding={padding} title={title} />
+    {cardsData.map(({ title, info, padding, source }) => (
+      <Card key={title} info={info} padding={padding} source={source} title={title} />
     ))}
   </Wrapper>
 )

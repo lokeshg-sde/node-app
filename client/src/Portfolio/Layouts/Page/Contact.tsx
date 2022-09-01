765815f4503b5fa055f0d3d911a215f82dcdd657
lokeshg-fsd <lokesh.jejappa@gmail.com>
2022-09-01 23:56:28 +0530

    Use Contact Form in Contact Section

--- client/src/Portfolio/Layouts/Page/Contact.tsx
+++ client/src/Portfolio/Layouts/Page/Contact.tsx
@@ -1,33 +1,12 @@
 import React from 'react'
 
-import {
-  Header,
-  Details,
-  Container,
-  Form,
-  Input,
-  TextArea,
-  Button,
-} from '../../Components/ContactSection'
+import { Header, Form } from '../../Components/ContactSection'
 
 export default function Contact(): JSX.Element {
   return (
     <>
       <Header />
-      <Container>
-        <Form method="POST" name="contact">
-          <Input name="name" placeholder="Full Name" required type="text" />
-          <div>
-            <Input name="email" placeholder="Email" required type="email" width="46%" />
-            <Input name="phone-number" placeholder="Phone Number" type="text" width="46%" />
-          </div>
-          <div>
-            <TextArea name="message" placeholder="Enter your message" required />
-            <Details />
-          </div>
-          <Button type="submit">Submit</Button>
-        </Form>
-      </Container>
+      <Form />
     </>
   )
 }

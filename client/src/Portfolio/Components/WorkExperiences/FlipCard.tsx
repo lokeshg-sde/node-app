1c8aafcf752efa0b6a2b329a4b0a5d27a455eba9
lokeshg-fsd <lokesh.jejappa@gmail.com>
2022-09-10 23:54:05 +0530

    Fix Style Issue On FlipFlop Cards and add Assets

--- client/src/Portfolio/Components/WorkExperiences/FlipCard.tsx
+++ client/src/Portfolio/Components/WorkExperiences/FlipCard.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import styled from '@emotion/styled'
 
+import { WorkCard } from './WorkCard'
+
 const Card = styled.div`
   background-color: transparent;
   width: 350px;
   height: 480px;
   perspective: 1000px;
   cursor: pointer;
-  :hover [data-test-id="inner-card"] {
+  :hover [data-test-id='inner-card'] {
     transform: rotateY(180deg);
   }
 `
@@ -18,7 +20,6 @@ const InnerCard = styled.div`
   text-align: center;
   transition: transform 1s;
   transform-style: preserve-3d;
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
 `
 
 const FrontSide = styled.div`
@@ -26,7 +27,6 @@ const FrontSide = styled.div`
   width: 100%;
   height: 100%;
   backface-visibility: hidden;
-  background-color: #bbb;
   color: black;
   z-index: 2;
   border-radius: 5px;
@@ -38,7 +38,6 @@ const BackSide = styled.div`
   width: 100%;
   height: 100%;
   backface-visibility: hidden;
-  background-color: #2980b9;
   color: white;
   transform: rotateY(180deg);
   z-index: 1;
@@ -49,10 +48,10 @@ export const FlipFlopCard = (): JSX.Element => (
   <Card>
     <InnerCard data-test-id="inner-card">
       <FrontSide>
-        <div>Front CARD </div>
+        <WorkCard />
       </FrontSide>
       <BackSide>
-        <div>Back CARD </div>
+        <WorkCard />
       </BackSide>
     </InnerCard>
   </Card>
